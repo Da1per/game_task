cf36907d6fbb16d9bf4f083bbd8be581d195a16a
dalper <dalper>
2023-11-22 23:53:38 +0300

    add

--- src/LeftSection/LeftSection.js
+++ src/LeftSection/LeftSection.js
@@ -1,29 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import "./leftsection.css"
 import { useSelector,useDispatch } from 'react-redux'
 const LeftSection = ()=>{
   let arrTask= useSelector((state) => state.reduceSlice.tasks)
-  let taskTabs=[]
-  let listTasks=()=>{ 
+  /* let taskTabs =[<div key={1}>dfdfdre45df</div>] */
+  const [taskTabs, setTaskTabs] = useState([]);
+  console.log(taskTabs)
+  
+  useEffect(() => {
+    for (let i = 0; i < arrTask.length; i++) {
+      
+      taskTabs.push(<div key={i}>{arrTask[i]}</div>)
+    }
     
-    for (let i=0;i>arrTask.length;i++){
-      console.log('ffff')
-      taskTabs.push(
-        <div id={i}>
-          Task is {i}
-          {arrTask[i]}
-        </div>
-      )
-    } 
-    return taskTabs
-    
-  }
-  listTasks()
-  console.log(listTasks)
+  },[arrTask])
+ 
     return (
     <div className='leftsection'>
         {taskTabs}
-        rgrg
+        
     </div>
   )
 }
